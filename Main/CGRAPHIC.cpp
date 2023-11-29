fee76536377dfbccc51073149014396adcb3e952
Ender OG <ngn2910pog@gmail.com>
2023-11-29 23:03:04 +0700

    Updated DrawPauseMenu in CGRAPHIC

--- Main/CGRAPHIC.cpp
+++ Main/CGRAPHIC.cpp
@@ -88,4 +88,77 @@ void CGRAPHIC::DrawObject(vector<wstring> contentsArr, int first_x, int first_y,
 	}
 }
 
-
+void CGRAPHIC::DrawPauseMenu(vector<wstring> PauseMenu, int first_x, int first_y) {
+	vector<wstring> frame = PAUSE_MENU;
+	//set buffer
+	for (int i = 0; i < 52; i++)
+		for (int j = 0; j < 30;	 j++)
+			screen[first_x + i][j] = { frame[j][i], BLACK, DARK_BLUE };
+	//set colors
+	for (int i = 0; i < 5; i++) {
+		screen[first_x + i][first_y + 0].bgdColor = -1;
+		screen[first_x + 47 + i][first_y + 0].bgdColor = -1;
+	}
+	screen[first_x+0][first_y + 1].bgdColor = -1;
+	screen[first_x + 1][first_y + 1].bgdColor = -1;
+	screen[first_x + 0][first_y + 2].bgdColor = -1;
+	screen[first_x+ 51][first_y + 1].bgdColor = -1;
+	screen[first_x + 50][first_y + 1].bgdColor = -1;
+	screen[first_x + 51][first_y + 2].bgdColor = -1;
+	for (int i = 0; i < 18; i++) {
+		screen[first_x + 17 + i][first_y + 0].bgdColor = LIGHT_GRAY;
+		screen[first_x + 17 + i][first_y + 1].bgdColor = LIGHT_GRAY;
+	}
+	for (int i = 4; i < 48; i++)
+		for (int j = 2; j < 28; j++)
+			screen[first_x + i][first_y + j].bgdColor = SAND;
+	screen[first_x + 47][first_y + 26].bgdColor = DARK_BLUE;
+	screen[first_x + 45][first_y + 27].bgdColor = DARK_BLUE;
+	screen[first_x + 46][first_y + 27].bgdColor = DARK_BLUE;
+	screen[first_x + 47][first_y + 27].bgdColor = DARK_BLUE;
+	for (int i = 7; i <= 14; i++)
+		for (int j = 10; j <= 13; j++)
+			screen[first_x + i][first_y + j].txtColor = DARK_GRAY;
+	for (int i = 24; i <= 28; i++)
+		for (int j = 10; j <= 13; j++)
+			screen[first_x + i][first_y + j].txtColor = DARK_GREEN;
+	for (int i = 39; i <= 43; i++)
+		for (int j = 10; j <= 13; j++)
+			screen[first_x + i][first_y + j].txtColor = BRIGHT_YELLOW;
+	for (int i = 15; i <= 22; i++)
+		for (int j = 19; j <= 22; j++)
+			screen[first_x + i][first_y + j].txtColor = DARK_BLUE;
+	screen[first_x + 16][first_y + 19].txtColor = LIGHT_GRAY;
+	screen[first_x + 17][first_y + 19].txtColor = WHITE;
+	screen[first_x + 19][first_y + 19].txtColor = LIGHT_GRAY;
+	screen[first_x + 16][first_y + 20].bgdColor = LIGHT_GRAY;
+	screen[first_x + 17][first_y + 20].bgdColor = LIGHT_GRAY;
+	screen[first_x + 19][first_y + 20].bgdColor = WHITE;
+	screen[first_x + 16][first_y + 21].bgdColor = LIGHT_GRAY;
+	screen[first_x + 17][first_y + 21].bgdColor = WHITE;
+	screen[first_x + 18][first_y + 21].bgdColor = WHITE;
+	screen[first_x + 19][first_y + 21].bgdColor = WHITE;
+	screen[first_x + 20][first_y + 21].bgdColor = LIGHT_GRAY;
+	screen[first_x + 16][first_y + 22].bgdColor = LIGHT_GRAY;
+	screen[first_x + 17][first_y + 22].bgdColor = LIGHT_GRAY;
+	screen[first_x + 18][first_y + 22].bgdColor = LIGHT_GRAY;
+	screen[first_x + 19][first_y + 22].bgdColor = WHITE;
+	screen[first_x + 20][first_y + 22].bgdColor = LIGHT_GRAY;
+	for (int i = 31; i <= 39; i++)
+		for (int j = 19; j <= 22; j++)
+			screen[first_x + i][first_y + j].txtColor = RED;
+	for (int i = 6; i <= 20; i++)
+		for (int j = 2; j <= 8; j++)
+			screen[first_x + i][first_y + j].bgdColor = WHITE;
+	for (int i = 10; i <= 18; i++)
+		for (int j = 3; j <= 5; j++)
+			screen[first_x + i][first_y + j].bgdColor = DARK_GREEN;
+	screen[first_x + 8][first_y + 6].bgdColor = DARK_GREEN;
+	screen[first_x + 9][first_y + 6].bgdColor = DARK_GREEN;
+	screen[first_x + 10][first_y + 6].bgdColor = DARK_GREEN;
+	screen[first_x + 11][first_y + 6].bgdColor = DARK_GREEN;
+	screen[first_x + 10][first_y + 7].bgdColor = DARK_GREEN;
+	screen[first_x + 12][first_y + 6].bgdColor = LIGHT_GREEN;
+	screen[first_x + 13][first_y + 6].bgdColor = LIGHT_GREEN;
+	screen[first_x + 14][first_y + 6].bgdColor = LIGHT_GREEN;
+}
